7a6d60994204eb75095f5e225e356a61d25fdf6a
rjsah09 <rjsah09@naver.com>
2025-04-10 23:14:16 +0900

    feat: base 속성 변경

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const base = process.env.VITE_BASE || '/';
-
 export default defineConfig({
-  base,
+  base: '/workshop-teambuilding',
   plugins: [vue()],
 });
