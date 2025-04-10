6daa4ca34c0b608203fba6b8e4a0af2646274f23
rjsah09 <rjsah09@naver.com>
2025-04-10 22:59:23 +0900

    fix: vite.config 중복 선언 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
 const base = process.env.VITE_BASE || '/';
 
 export default defineConfig({
